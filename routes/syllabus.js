5706eb8aef47eed6df14c6f0b4af2381729f2b27
Avinash <avinashkumar.7@outlook.com>
2026-03-21 22:55:46 +0530

    fix date and planned executed final

--- routes/syllabus.js
+++ routes/syllabus.js
@@ -17,22 +17,35 @@ const { course, year } = req.body;
   const workbook = XLSX.readFile(req.file.path);
   const sheet = workbook.Sheets[workbook.SheetNames[0]];
 
+  function excelDateToJSDate(serial) {
+  if (!serial) return "";
+  const utc_days = Math.floor(serial - 25569);
+  const utc_value = utc_days * 86400;                                        
+  const date_info = new Date(utc_value * 1000);
+
+  const day = date_info.getDate().toString().padStart(2, '0');
+  const month = (date_info.getMonth() + 1).toString().padStart(2, '0');
+  const year = date_info.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
+
   const raw = XLSX.utils.sheet_to_json(sheet, { range: 1 });
 
   const data = raw.map((row, index) => ({
     sn: row["S.N"] || index + 1,
     subject: row["Subject"] || "",
     faculty: row["Faculty"] || "",
-    startDate: row["Start Date"] || "",
-    endDate: row["End Date"] || "",
-    planned: Number(row["Planned"]) || 0,
-    executed: Number(row["Executed"]) || 0,
+startDate: excelDateToJSDate(row["Start Date"]),
+endDate: excelDateToJSDate(row["End Date"]),
+planned: Number(row["Planned"] || row["Planned Classes"] || row["No. of classes Planned"]) || 0,
+executed: Number(row["Executed"] || row["Executed Classes"] || row["No. of Classes Executed"]) || 0,
     status: row["Status"] || "",
     course: course,
   year: year        
   }));
 
-  await mongoose.connection.collection("syllabus").deleteMany({});
+  await mongoose.connection.collection("syllabus").deleteMany({ course });
   await mongoose.connection.collection("syllabus").insertMany(data);
 
   res.redirect("/admin/syllabus");
