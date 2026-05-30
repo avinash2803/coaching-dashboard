622b68c00a686d73b89b03d41e26bc20b24fd4f3
Avinash <avinashkumar.7@outlook.com>
2026-05-31 02:06:54 +0530

    Fix analytics render data

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -6,9 +6,35 @@ router.get("/", async (req, res) => {
 
   try {
 
+    const testAnalytics = [
+
+      {
+
+        testName: "Test-1",
+
+        subject: "Indian History + Polity",
+
+        fullMarks: 200,
+
+        candidatesAppeared: 82,
+
+        absentStudents: 14,
+
+        averageScore: 94,
+
+        averagePercentage: 47,
+
+        above50: 26,
+
+        topperName: "Ravi Kumar",
+
+        topScore: 188
+      }
+    ];
+
     res.render("analytics", {
 
-      testAnalytics: []
+      testAnalytics
     });
 
   } catch (error) {
